3a2c346b14f9d6f55128b80212e5f5a1f842a950
Steven Rodriguez <srrodrig.rodriguez@gmail.com>
2020-12-10 20:26:35 -0600

    Added initial files

---
+++ build/static/js/main.54eb7932.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpdirectory=this.webpackJsonpdirectory||[]).push([[0],{10:function(e,r,t){"use strict";t.r(r);var n=t(0),c=(t(1),t(2)),i=t.n(c);var s=function(){return Object(c.render)(),Object(n.jsx)("div",{children:Object(n.jsx)("header",{children:"TEST"})})};var d=function(){return Object(n.jsx)(s,{})};t(9);i.a.render(Object(n.jsx)(d,{}),document.getElementById("root"))}},[[10,1,2]]]);
+//# sourceMappingURL=main.54eb7932.chunk.js.map
