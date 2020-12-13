d92fe01d1d037f0a41e3e6163e7b2591c9859fd7
Steven Rodriguez <srrodrig.rodriguez@gmail.com>
2020-12-12 23:44:15 -0600

    Adding deploy

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import Portfolio from "./pages/Portfolio";
-// import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
 import "./App.css";
@@ -14,7 +13,6 @@ function App() {
         <Router>
             <div>
             <Wrapper>
-                {/* <Navbar /> */}
                 <Route exact path="/" component={Home} />
                 <Route exact path="/contact" component={Contact} />
                 <Route exact path="/portfolio" component={Portfolio} />
